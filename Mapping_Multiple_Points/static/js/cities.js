262f2fe47e441856b1f084cb3d18ce3fec6b2371
forrestcasey <forrestpcasey@gmail.com>
2021-12-16 17:25:31 -0600

    13.4.2 map multiple points complete

---
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -0,0 +1,38 @@
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//  13.4.2 -- replace the marker variable (which we used to map one location) 
+// with the cities variable that references the five most populous cities array 
+// in the following code block:
+
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
