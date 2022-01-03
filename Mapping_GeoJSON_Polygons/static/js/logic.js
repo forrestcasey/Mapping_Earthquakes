fd002e16156e030bc0105cedd5b0d2bee3017eb6
forrestcasey <forrestpcasey@gmail.com>
2022-01-02 22:17:42 -0600

    mapping polygons 13.5.6

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,42 @@
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
+});
+
+// Pass our map layers into our layer control and add the layer control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/forrestcasey/Mapping_Earthquakes/main/Mapping_GeoJSON_Points/static/data/torontoNeighborhoods.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+        console.log(data);
+    // creating a GeoJSON layer with the retrived data.
+    L.geoJson(data).addTo(map);
+});
+
